591078c364ddf89fcd4ed866b12354a149ef3096
Liumxiris <github@mliris.com>
2024-05-12 19:37:46 -0400

    refactor code

--- pages/C_Train_Model.py
+++ pages/C_Train_Model.py
@@ -25,71 +25,13 @@ from sklearn.metrics import (
 )
 from sklearn.metrics import accuracy_score
 
-if "processed_df" in st.session_state:
-    mbti_data = st.session_state.processed_df
-    row_count = st.slider("Select number of rows to view", 1, len(mbti_data), 100)
-    st.dataframe(mbti_data.head(row_count))
-
-# ----------------------------------------------
-# Feature Engineering
-# ----------------------------------------------
-
-new_df = st.session_state.processed_df
-new_df["posts"] = new_df["posts"].apply(
-    lambda x: x[:100] + "..." if len(x) > 100 else x
-)
-new_df.head()
-
-# Converting MBTI personality into numerical form using Label Encoding
-enc = LabelEncoder()
-new_df["type of encoding"] = enc.fit_transform(new_df["type"])
-
-target = new_df["type of encoding"]
-
-le_name_mapping = dict(zip(enc.classes_, enc.fit_transform(enc.classes_)))
-new_dict = dict([(value, key) for key, value in le_name_mapping.items()])
-new_dict
-
-
-nltk.download("stopwords")
-nltk.download("punkt")
-nltk.download("wordnet")
-print(stopwords.words("english"))
-
-
 class Lemmatizer:
     def __init__(self):
         self.lemmatizer = WordNetLemmatizer()
 
     def __call__(self, text):
         return [self.lemmatizer.lemmatize(word) for word in word_tokenize(text)]
-
-
-# Vectorizing the posts for the model and filtering Stop-words
-vect = CountVectorizer(max_features=10000, stop_words="english", tokenizer=Lemmatizer())
-
-# Converting posts (or training or X feature) into numerical form by count vectorization
-train = vect.fit_transform(new_df["posts"])
-
-print(train.shape)
-
-
-# ----------------------------------------------
-# Data-splitting: 70-30
-# ----------------------------------------------
-
-X_train, X_test, y_train, y_test = train_test_split(
-    train, target, test_size=0.3, stratify=target, random_state=42
-)
-print((X_train.shape), (y_train.shape), (X_test.shape), (y_test.shape))
-
-
-# ----------------------------------------------
-# Logistic Regression
-# ----------------------------------------------
-
-st.header('Logistic Regression')
-
+    
 class LogisticRegression_jamie(object):
     def sigmoid(self,z):
         sig = 1/(1+exp(-z))
@@ -132,6 +74,7 @@ class LogisticRegression_jamie(object):
             cost_list[i] = cost(weights)
         self.weights = weights
         return cost_list
+    
     def predict(self,X):
         z = dot(self.initialize(X)[1],self.weights)
         lis = []
@@ -141,167 +84,13 @@ class LogisticRegression_jamie(object):
             else:
                 lis.append(0)
         return lis
-
-
-lr_value = st.slider("Select a learning rate", min_value=0.01, max_value=0.1, step=0.01)
-st.write("Logistic Regression Learning Rate:", lr_value)
-
-num_of_iter = st.slider("Select number of iterations", min_value=100, max_value=1000, step=100)
-st.write("Logistic Regression Number of Iterations:", num_of_iter)
-
-if st.button("Predict"):
-    accuracies = {}
-    logreg_model = LogisticRegression_jamie()
-    logreg_model.fit(X_train, y_train,alpha=lr_value, iter=num_of_iter)
-    Y_pred = logreg_model.predict(X_test)
-    predictions = [round(value) for value in Y_pred]
-
-    # evaluate predictions
-    accuracy = accuracy_score(y_test, predictions)
-    accuracies['Logistic Regression'] = accuracy* 100.0
-    st.write("Accuracy: %.2f%%" % (accuracy * 100.0))
-
-
-# ----------------------------------------------
-# KNN
-# ----------------------------------------------
-
-
-# Three distance metrics for KNN
-def euclidean(v1, v2):
-    # Euclidean distance (l2 norm)
-    return np.sqrt(np.sum((v1 - v2) ** 2))
-
-
-def manhattan(v1, v2):
-    # Manhattan distance (l1 norm)
-    return np.sum(np.abs(v1 - v2))
-
-
-def minkowski(v1, v2, p=2):
-    # Minkowski distance (lp norm)
-    return np.sum(np.abs(v1 - v2) ** p) ** (1 / p)
-
-
-def cosine(v1, v2):
-    # Cosine similarity
-    return np.dot(v1, v2) / (normalize(v1) * normalize(v2))
-
-
-def normalize(X):
-    means = np.mean(X, axis=0)  # columnwise mean and std
-    stds = np.std(X, axis=0) + 1e-7
-    X_normalized = (X - means) / (stds)
-    return X_normalized
-
-
-# Get nearest neighbours
-def get_neighbours(X, test_row, Y, k, p, metric):
-    """
-    Returns k nearest neighbors
-    """
-    distances_ = []
-    neigh_class = []
-
-    # Calculate distance to all points in X_train
-    for train_row, train_class in zip(X, Y):
-        dist = 0
-        if metric == "euclidean":
-            dist = euclidean(train_row, test_row)
-        elif metric == "manhattan":
-            dist = manhattan(train_row, test_row)
-        elif metric == "minkowski":
-            dist = minkowski(train_row, test_row, p)
-        elif metric == "cosine":
-            dist = minkowski(train_row, test_row, p)
-        else:
-            raise NameError(
-                "Supported metrics are euclidean, manhattan, cosine, and minkowski"
-            )
-        distances_.append(dist)
-        neigh_class.append(train_class)
-
-    # Sort distances
-    sorted_ind = np.argsort(distances_, axis=0)  # by rows
-
-    # Identify k nearest neighbours
-    sorted_dist = [distances_[i] for i in sorted_ind][:k]
-    neighbours_class = [neigh_class[i] for i in sorted_ind][:k]
-
-    return neighbours_class, sorted_dist
-
-
-def predict(X_train, X_test, Y, k, p, metric):
-    """
-    Make predictions
-    """
-    kclusters = []
-    distances = []
-    # Loop over rows in test set
-    for test_row in X_test:
-        nearest_neighbours, n_dist = get_neighbours(X_train, test_row, Y, k, p, metric)
-        kclusters.append(nearest_neighbours)
-        distances.append(n_dist)
-    return np.array(kclusters), np.array(distances)
-
-
-# Hyper-parameters
-num_neighhbors = 15
-metric = "manhattan"  # Options: 'manhattan', 'minkowski', 'euclidean', 'cosine'
-p = 2  # 1 for l1-norm; 2 for l2-norm
-
-X_train = pd.DataFrame(X_train.toarray())
-X_test = pd.DataFrame(X_test.toarray())
-# y_train = pd.DataFrame(y_train.toarray())
-# y_test = pd.DataFrame(y_test.toarray())
-
-
-# Use subsets to speed up predicting
-n_start_articles = 0
-n_end_articles = 200
-
-X_train_subset = X_train[n_start_articles:n_end_articles]
-y_train_subset = np.array(y_train[n_start_articles:n_end_articles])
-
-X_test_subset = X_test[n_start_articles:n_end_articles]
-y_test_subset = np.array(y_test[n_start_articles:n_end_articles])
-
-
-neighbours_class, cluster_distances = predict(
-    X_train_subset.to_numpy(),
-    X_train_subset.to_numpy(),
-    y_train_subset,
-    num_neighhbors,
-    p,
-    metric,
-)
-neighbours_class = neighbours_class[
-    :, 1:
-]  # removing the closest neighbor which is the point itself
-print(
-    "The nearest neighbor clusters distances are: \n{}".format(cluster_distances[:5, :])
-)
-print("The nearest neighbor clusters predictions: \n{}".format(neighbours_class[:5, :]))
-
-
-# Finding the dominant class in each row of neighbor classes
-cluster_predictions, counts = mode(neighbours_class, axis=1)
-print(cluster_predictions)
-print(y_test_subset[0], cluster_predictions[0])
-
-# showing accuracy and f1_scores
-models_accuracy = accuracy_score(y_test_subset, cluster_predictions)
-f1_scores = f1_score(y_test_subset, cluster_predictions, average="macro")
-print(models_accuracy, f1_scores)
-
-
-# logistic regression with SGD
+        
+   # logistic regression with SGD
 class LogisticRegression(object):
     def __init__(self, learning_rate=0.001, num_iterations=500):
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
         self.likelihood_history = []
-
 
     def predict_probability(self, X):
         '''
@@ -322,7 +111,6 @@ class LogisticRegression(object):
         y_pred = 1. / (1. + np.exp(-score))
 
         return y_pred
-
 
     def compute_avg_log_likelihood(self, X, Y, W):
         '''
@@ -345,7 +133,6 @@ class LogisticRegression(object):
 
         lp = np.sum((indicator - 1) * scores - logexp) / len(X)
         return lp
-
 
     def update_weights(self):
         '''
@@ -394,7 +181,6 @@ class LogisticRegression(object):
         except ValueError as err:
             st.write({str(err)})
         return y_pred
-
 
     def fit(self, X, Y):
         '''
@@ -492,16 +278,16 @@ class StochasticLogisticRegression(LogisticRegression):
                 self.W[j] -= self.learning_rate * dW
 
             lp = self.compute_avg_log_likelihood(self.X[i:i + self.batch_size, :], self.Y[i:i + self.batch_size],
-                                                 self.W)
+                                                self.W)
 
             self.likelihood_history.append(lp)
             if itr <= 15 or (itr <= 1000 and itr % 100 == 0) or (itr <= 10000 and itr % 1000 == 0) \
                     or itr % 10000 == 0 or itr == self.num_iterations - 1:
                 data_size = len(X)
                 print('Iteration %*d: Average log likelihood (of data points in batch [%0*d:%0*d]) = %.8f' % \
-                      (int(np.ceil(np.log10(self.num_iterations))), itr, \
-                       int(np.ceil(np.log10(data_size))), i, \
-                       int(np.ceil(np.log10(data_size))), i + self.batch_size, lp))
+                    (int(np.ceil(np.log10(self.num_iterations))), itr, \
+                    int(np.ceil(np.log10(data_size))), i, \
+                    int(np.ceil(np.log10(data_size))), i + self.batch_size, lp))
 
             i += self.batch_size
             if i + self.batch_size > len(self.X):
@@ -511,21 +297,163 @@ class StochasticLogisticRegression(LogisticRegression):
                 i = 0
             self.learning_rate = self.learning_rate / 1.02
 
+# def vetorize_posts(df):
+#     # Vectorizing the posts for the model and filtering Stop-words
+#     vect = CountVectorizer(max_features=10000, stop_words="english", tokenizer=Lemmatizer())
 
+#     # Converting posts (or training or X feature) into numerical form by count vectorization
+#     train = vect.fit_transform(df)
+#     return train
 
-# select options for model
-model_options = ['Logistic Regression', 'Stochastic Gradient Descent with Logistic Regression', 'KNN']
-model_select = st.multiselect(
-    label='Select model for prediction',
-    options=model_options,
-)
-st.write('You selected : {}'.format(
-    model_select))
+if "processed_df" in st.session_state:
+    new_df = st.session_state.processed_df
 
-if st.button('Train Model'):
+    # ----------------------------------------------
+    # Feature Engineering
+    # ----------------------------------------------
+
+    new_df["posts"] = new_df["posts"].apply(
+        lambda x: x[:100] + "..." if len(x) > 100 else x
+    )
+    new_df.head()
+
+    # Converting MBTI personality into numerical form using Label Encoding
+    enc = LabelEncoder()
+    new_df["type of encoding"] = enc.fit_transform(new_df["type"])
+
+    target = new_df["type of encoding"]
+
+    le_name_mapping = dict(zip(enc.classes_, enc.fit_transform(enc.classes_)))
+    new_dict = dict([(value, key) for key, value in le_name_mapping.items()])
+
+    nltk.download("stopwords")
+    nltk.download("punkt")
+    nltk.download("wordnet")
+    print(stopwords.words("english"))
+
+    # Vectorizing the posts for the model and filtering Stop-words
+    vect = CountVectorizer(max_features=10000, stop_words="english", tokenizer=Lemmatizer())
+
+    # Converting posts (or training or X feature) into numerical form by count vectorization
+    train = vect.fit_transform(new_df["posts"])
+    # train = vetorize_posts(new_df["posts"])
+
+    print(train.shape)
+
+    # ----------------------------------------------
+    # Data-splitting: 70-30
+    # ----------------------------------------------
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        train, target, test_size=0.3, stratify=target, random_state=42
+    )
+    print((X_train.shape), (y_train.shape), (X_test.shape), (y_test.shape))
+
+    # Three distance metrics for KNN
+    def euclidean(v1, v2):
+        # Euclidean distance (l2 norm)
+        return np.sqrt(np.sum((v1 - v2) ** 2))
+
+    def manhattan(v1, v2):
+        # Manhattan distance (l1 norm)
+        return np.sum(np.abs(v1 - v2))
+
+    def minkowski(v1, v2, p=2):
+        # Minkowski distance (lp norm)
+        return np.sum(np.abs(v1 - v2) ** p) ** (1 / p)
+
+    def cosine(v1, v2):
+        # Cosine similarity
+        return np.dot(v1, v2) / (normalize(v1) * normalize(v2))
+
+    def normalize(X):
+        means = np.mean(X, axis=0)  # columnwise mean and std
+        stds = np.std(X, axis=0) + 1e-7
+        X_normalized = (X - means) / (stds)
+        return X_normalized
+
+    # Get nearest neighbours
+    def get_neighbours(X, test_row, Y, k, p, metric):
+        """
+        Returns k nearest neighbors
+        """
+        distances_ = []
+        neigh_class = []
+
+        # Calculate distance to all points in X_train
+        for train_row, train_class in zip(X, Y):
+            dist = 0
+            if metric == "euclidean":
+                dist = euclidean(train_row, test_row)
+            elif metric == "manhattan":
+                dist = manhattan(train_row, test_row)
+            elif metric == "minkowski":
+                dist = minkowski(train_row, test_row, p)
+            elif metric == "cosine":
+                dist = minkowski(train_row, test_row, p)
+            else:
+                raise NameError(
+                    "Supported metrics are euclidean, manhattan, cosine, and minkowski"
+                )
+            distances_.append(dist)
+            neigh_class.append(train_class)
+
+        # Sort distances
+        sorted_ind = np.argsort(distances_, axis=0)  # by rows
+
+        # Identify k nearest neighbours
+        sorted_dist = [distances_[i] for i in sorted_ind][:k]
+        neighbours_class = [neigh_class[i] for i in sorted_ind][:k]
+
+        return neighbours_class, sorted_dist
+
+    def predict(X_train, X_test, Y, k, p, metric):
+        """
+        Make predictions
+        """
+        kclusters = []
+        distances = []
+        # Loop over rows in test set
+        for test_row in X_test:
+            nearest_neighbours, n_dist = get_neighbours(X_train, test_row, Y, k, p, metric)
+            kclusters.append(nearest_neighbours)
+            distances.append(n_dist)
+        return np.array(kclusters), np.array(distances)
+
+    # select options for model
+    model_options = ['Logistic Regression', 'Stochastic Gradient Descent with Logistic Regression', 'KNN']
+    model_select = st.multiselect(
+        label='Select model for prediction',
+        options=model_options,
+    )
+    st.write('You selected : {}'.format(
+        model_select))
+    
+    # ----------------------------------------------
+    # Logistic Regression
+    # ----------------------------------------------
+
+    if (model_options[0] in model_select):
+        st.header('Logistic Regression')
+        lr_value = st.slider("Select a learning rate", min_value=0.01, max_value=0.1, step=0.01)
+        st.write("Logistic Regression Learning Rate:", lr_value)
+
+        num_of_iter = st.slider("Select number of iterations", min_value=100, max_value=1000, step=100)
+        st.write("Logistic Regression Number of Iterations:", num_of_iter)
+        if st.button("Train Model"):
+            accuracies = {}
+            logreg_model = LogisticRegression_jamie()
+            logreg_model.fit(X_train, y_train,alpha=lr_value, iter=num_of_iter)
+            Y_pred = logreg_model.predict(X_test)
+            predictions = [round(value) for value in Y_pred]
+
+            # evaluate predictions
+            accuracy = accuracy_score(y_test, predictions)
+            accuracies['Logistic Regression'] = accuracy* 100.0
+            st.write("Accuracy: %.2f%%" % (accuracy * 100.0))            
+
     if (model_options[1] in model_select):
         st.markdown('#### ' + model_options[1])
-
         # Number of iterations: maximum iterations to run the iterative SGD
         sdg_num_iterations = st.number_input(
             label='Enter the number of maximum iterations on training data',
@@ -563,8 +491,8 @@ if st.button('Train Model'):
 
         try:
             sgd = StochasticLogisticRegression(num_iterations=sgd_params['num_iterations'],
-                                               learning_rate=sgd_params['learning_rate'],
-                                               batch_size=sgd_params['batch_size'])
+                                            learning_rate=sgd_params['learning_rate'],
+                                            batch_size=sgd_params['batch_size'])
             sgd.fit(X_train.to_numpy(), np.ravel(y_train))
             st.session_state[model_options[1]] = sgd
             y_pred_sgd = sgd.predict(X_test.to_numpy())
@@ -572,3 +500,58 @@ if st.button('Train Model'):
             st.write(f"Accuracy: {accuracy * 100.0}%")
         except ValueError as err:
             st.write({str(err)})
+
+    # ----------------------------------------------
+    # KNN
+    # ----------------------------------------------
+
+    if (model_options[2] in model_select):
+        # Hyper-parameters
+        num_neighhbors = 15
+        metric = "manhattan"  # Options: 'manhattan', 'minkowski', 'euclidean', 'cosine'
+        p = 2  # 1 for l1-norm; 2 for l2-norm
+
+        X_train = pd.DataFrame(X_train.toarray())
+        X_test = pd.DataFrame(X_test.toarray())
+        # y_train = pd.DataFrame(y_train.toarray())
+        # y_test = pd.DataFrame(y_test.toarray())
+
+
+        # Use subsets to speed up predicting
+        n_start_articles = 0
+        n_end_articles = 200
+
+        X_train_subset = X_train[n_start_articles:n_end_articles]
+        y_train_subset = np.array(y_train[n_start_articles:n_end_articles])
+
+        X_test_subset = X_test[n_start_articles:n_end_articles]
+        y_test_subset = np.array(y_test[n_start_articles:n_end_articles])
+
+
+        neighbours_class, cluster_distances = predict(
+            X_train_subset.to_numpy(),
+            X_train_subset.to_numpy(),
+            y_train_subset,
+            num_neighhbors,
+            p,
+            metric,
+        )
+        neighbours_class = neighbours_class[
+            :, 1:
+        ]  # removing the closest neighbor which is the point itself
+        print(
+            "The nearest neighbor clusters distances are: \n{}".format(cluster_distances[:5, :])
+        )
+        print("The nearest neighbor clusters predictions: \n{}".format(neighbours_class[:5, :]))
+
+
+        # Finding the dominant class in each row of neighbor classes
+        cluster_predictions, counts = mode(neighbours_class, axis=1)
+        print(cluster_predictions)
+        print(y_test_subset[0], cluster_predictions[0])
+
+        # showing accuracy and f1_scores
+        models_accuracy = accuracy_score(y_test_subset, cluster_predictions)
+        f1_scores = f1_score(y_test_subset, cluster_predictions, average="macro")
+        print(models_accuracy, f1_scores)
+
